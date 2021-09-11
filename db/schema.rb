3c96523667d160d3b4cafa512547791d212aca84
Werner <inqast@gmail.com>
2021-09-11 21:35:13 +0300

    [feat] migrations for comments added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_07_194906) do
+ActiveRecord::Schema.define(version: 2021_09_10_173850) do
 
   create_table "post_categories", force: :cascade do |t|
     t.string "name"
@@ -18,13 +18,25 @@ ActiveRecord::Schema.define(version: 2021_09_07_194906) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "post_comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_post_comments_on_ancestry"
+    t.index ["post_id"], name: "index_post_comments_on_post_id"
+    t.index ["user_id"], name: "index_post_comments_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-    t.integer "post_category_id"
+    t.integer "user_id", null: false
+    t.integer "post_category_id", null: false
     t.index ["post_category_id"], name: "index_posts_on_post_category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
@@ -41,6 +53,8 @@ ActiveRecord::Schema.define(version: 2021_09_07_194906) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "post_comments", "posts"
+  add_foreign_key "post_comments", "users"
   add_foreign_key "posts", "post_categories"
   add_foreign_key "posts", "users"
 end
