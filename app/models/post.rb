c08079d913f24b39d9a03238326caf3f5fb08309
Werner <inqast@gmail.com>
2021-09-12 17:03:10 +0300

    [feat] model for likes implemented

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
   belongs_to :post_category, inverse_of: :posts
 
   has_many :comments, class_name: 'PostComment', inverse_of: :post, dependent: :destroy
+  has_many :likes, class_name: 'PostLike', inverse_of: :post, dependent: :destroy
 
   validates :title, :body, presence: true
 end
