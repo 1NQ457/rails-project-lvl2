510b988c7c2df6e4dfc2ddf98a8a7f63fddc509e
Werner <inqast@gmail.com>
2021-09-26 13:26:55 +0300

    [refactor] diff assertion added to like test

--- test/controllers/posts/likes_controller_test.rb
+++ test/controllers/posts/likes_controller_test.rb
@@ -11,8 +11,10 @@ class Posts::LikesControllerTest < ActionDispatch::IntegrationTest
   test 'should_create' do
     post = posts(:two)
 
-    assert post post_likes_path(post)
-    assert_equal 2, post.reload.likes.size
+    assert_difference('PostLike.count') do
+      post post_likes_path(post)
+    end
+
     assert_redirected_to post_path(post)
   end
 
@@ -20,8 +22,9 @@ class Posts::LikesControllerTest < ActionDispatch::IntegrationTest
     like = post_likes(:one)
     post = posts(:one)
 
-    assert delete post_like_path(post, like)
-    assert_equal 0, post.reload.likes.size
+    assert_difference('PostLike.count', -1) do
+      delete post_like_path(post, like)
+    end
     assert_redirected_to post_path(post)
   end
 end
